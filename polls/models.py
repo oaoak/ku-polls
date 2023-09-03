0cb39af94c1f399b4750a3ac33f9e4759d30bceb
KhongnatBaibua <khongnat.b@ku.th>
2023-09-03 18:11:07 +0700

    Add docstrings and fix indents

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,11 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
+    """
+    Question model with date published
+    """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
@@ -13,9 +17,15 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
 class Choice(models.Model):
+    """
+    Choice model that act as options for Question
+    """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
